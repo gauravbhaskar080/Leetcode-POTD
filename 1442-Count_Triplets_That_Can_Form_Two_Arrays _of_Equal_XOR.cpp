8def0d9169836fe72b5a34f6d437425a4f03a8f6
gauravbhaskar080 <gauravbhaskar111@gmail.com>
2024-05-31 00:40:21 +0530

    1442-3rd , 4th solution

--- 1442-Count_Triplets_That_Can_Form_Two_Arrays _of_Equal_XOR.cpp
+++ 1442-Count_Triplets_That_Can_Form_Two_Arrays _of_Equal_XOR.cpp
@@ -1,5 +1,5 @@
-#include<bits/stdc++.h>
-#include<iostream>
+#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 /*
 First Approach
@@ -7,18 +7,23 @@ TC : O(n^3)
 SC : O(1)
 */
 
-int countTriplets1(vector<int>&arr){
-    int n=arr.size();
-    int count=0,xora,xorb;
+int countTriplets1(vector<int> &arr)
+{
+    int n = arr.size();
+    int count = 0, xora, xorb;
 
-    for(int i=0;i<n;i++){
-        xora=0;
-        for(int j=i+1;j<n;j++){
-            xora = xora ^ arr[j-1]; //xora for(i to j-1)
+    for (int i = 0; i < n; i++)
+    {
+        xora = 0;
+        for (int j = i + 1; j < n; j++)
+        {
+            xora = xora ^ arr[j - 1]; // xora for(i to j-1)
             xorb = 0;
-            for(int k=j;k<n;k++){
-                xorb = xorb ^ arr[k]; //xorb for(j to k)
-                if(xora == xorb){
+            for (int k = j; k < n; k++)
+            {
+                xorb = xorb ^ arr[k]; // xorb for(j to k)
+                if (xora == xorb)
+                {
                     count++;
                 }
             }
@@ -33,9 +38,10 @@ TC : O(n^2)
 SC : O(n)
 */
 
-int countTriplets2(vector<int>&arr){
-    int n=arr.size(),ans=0;
-    vector<int> prefixXor(n+1,0);
+int countTriplets2(vector<int> &arr)
+{
+    int n = arr.size(), ans = 0;
+    vector<int> prefixXor(n + 1, 0);
     /*
     arr =         [2 3 1 6 7]
     prefixXor = [0 2 1 0 6 1]
@@ -44,26 +50,90 @@ int countTriplets2(vector<int>&arr){
                      i     k  ans  = 2 + ( 5 - (2+1)) = 2+2 = 4;  prefixXor[i] == prefixXor[k]
                        i   k  ans  = 4;
                          i k  ans  = 4;
-    */                   
-    for(int i=0;i<n;i++){
-        prefixXor[i+1] = prefixXor[i]^arr[i];
+    */
+    for (int i = 0; i < n; i++)
+    {
+        prefixXor[i + 1] = prefixXor[i] ^ arr[i];
     }
 
-    for(int i=0;i<n;i++){
-        for(int k=i+1;k<n;k++){
-            if(prefixXor[i] == prefixXor[k]){
-                ans += k + (i+1);
+    for (int i = 0; i < n; i++)
+    {
+        for (int k = i + 1; k < n; k++)
+        {
+            if (prefixXor[i] == prefixXor[k])
+            {
+                ans += k + (i + 1);
             }
         }
     }
     return ans;
 }
 
+/*
+Third Approach - Two Pass
+TC : O(n)
+SC : O(n)
+*/
 
-int main(){
-    vector<int> arr = {2,3,1,6,7};
-    cout<<"First Approach = "<<countTriplets1(arr)<<endl;
-    cout<<"Second Approach = "<<countTriplets2(arr)<<endl;
+int countTriplets3(vector<int> &arr)
+{
+    int n = arr.size(), ans = 0;
+    vector<int> prefixXor(n + 1, 0);
+    for (int i = 0; i < n; i++)
+    {
+        prefixXor[i + 1] = prefixXor[i] ^ arr[i];
+    }
+
+    unordered_map<int, int> sumOfi, countOfXor;
+    sumOfi[0] = 0; countOfXor[0] = 1;
+
+    for (int k = 1; k <= n; k++)
+    {
+        int x = prefixXor[k];
+        ans+= countOfXor[x]*k - sumOfi[x] - countOfXor[x]*1;
+
+        countOfXor[x]++;
+        sumOfi[x] += k; 
+    }
+
+    return ans;
+}
+
+
+/*
+Fourth Approach - One Pass
+TC : O(n)
+SC : O(n)
+*/
+
+int countTriplets4(vector<int> &arr)
+{
+    int n = arr.size(), ans = 0;
+    vector<int> prefixXor(n + 1, 0);
+
+    unordered_map<int, int> sumOfi, countOfXor;
+    sumOfi[0] = 0; countOfXor[0] = 1;
+
+    for (int k = 1; k <= n; k++)
+    {
+        prefixXor[k] = prefixXor[k-1] ^ arr[k-1];
+        int x = prefixXor[k];
+        ans+= countOfXor[x]*k - sumOfi[x] - countOfXor[x]*1;
+
+        countOfXor[x]++;
+        sumOfi[x] += k; 
+    }
+
+    return ans;
+}
+
+int main()
+{
+    vector<int> arr = {2, 3, 1, 6, 7};
+    cout << "First Approach = " << countTriplets1(arr) << endl;
+    cout << "Second Approach = " << countTriplets2(arr) << endl;
+    cout << "Third Approach = " << countTriplets3(arr) << endl;
+    cout << "Fourth Approach = " << countTriplets4(arr) << endl;
     return 0;
 }
 
@@ -72,4 +142,3 @@ Input: arr = [2,3,1,6,7]
 Output: 4
 Explanation: The triplets are (0,1,2), (0,2,2), (2,3,4) and (2,4,4)
 */
-
